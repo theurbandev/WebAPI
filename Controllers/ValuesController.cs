e3b3f55b99fa7197d1fe0e703580a083e2342461
Matthew Joseph <matthew.j.joe@gmail.com>
2022-01-25 21:37:27 -0500

    added controller, model, and repository

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebAPI.Models;
+using WebAPI.Repositories;
 
 namespace WebAPI.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    [Route("items")]
+    public class ItemsContoller : ControllerBase
     {
+        private readonly InMemRepository repository;
+
+        public ItemsContoller()
+        {
+            repository = new InMemRepository();
+        }
+
+
+        [HttpGet] // GET /items 
+        public IEnumerable<Item> GetItems()
+        {
+            var items = repository.GetItems();
+            return items;
+        }
     }
 }
---
+++ Repositories/InMemRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using WebAPI.Models;
+
+namespace WebAPI.Repositories
+{
+    public class InMemRepository
+    {
+        private readonly List<Item> items = new()
+        {
+            new Item { Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Shield", Price = 10, CreatedDate = DateTimeOffset.UtcNow }
+        };
+
+        public IEnumerable<Item> GetItems() // returns a list of all avaiable items
+        {
+            return items;
+        }
+
+        public Item GetItem(Guid id) // gets a specific items (needs id)
+        {
+            return items.Where(item => item.Id == id).SingleOrDefault();
+        }
+    }
+    
+}
