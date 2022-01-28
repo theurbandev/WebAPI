ac4a22d719c1e9564e3034b87edbf9b1fc14fd8c
Matthew Joseph <matthew.j.joe@gmail.com>
2022-01-28 01:40:58 -0500

    .NET 6 does not use startup.cs anymore - added DI to program.cs instead

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
+using Microsoft.Extensions.DependencyInjection;
+using WebAPI.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllersWithViews();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient(); // Add HttpClient here
+builder.Services.AddSingleton<IItemsRepository>(new InMemRepository(){});
 
 var app = builder.Build();
 
